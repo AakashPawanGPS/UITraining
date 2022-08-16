86daa06f25b439d93708909e4921eae3264d219b
AakashPawanGPS <aakashpawangps@gmail.com>
2022-08-16 21:27:37 +0530

    Blog Post Task Completed

---
+++ BlogPost/script.js
@@ -0,0 +1,18 @@
+function addPost(){
+    var text = document.getElementById("title").value
+    var title = document.createElement('h3')
+    title.innerHTML=text
+    document.getElementById("main-content").appendChild(title)
+
+    var d = document.getElementById("description").value
+    var des = document.createElement('p')
+    des.innerHTML=d
+    document.getElementById("main-content").appendChild(des)
+
+    var line = document.createElement('hr')
+    document.getElementById("main-content").appendChild(line)
+
+    document.getElementById("title").value=""
+    document.getElementById("description").value=""
+}
+
