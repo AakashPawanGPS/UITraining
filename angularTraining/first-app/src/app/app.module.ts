9d9063e028a380529ecbe728d97e730c4a9970c5
AakashPawanGPS <aakashpawangps@gmail.com>
2022-09-05 11:52:04 +0530

    Binding Tutorial

--- angularTraining/first-app/src/app/app.module.ts
+++ angularTraining/first-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DirectivesComponent } from './directives/directives.component';
+import { NewCompComponent } from './new-comp/new-comp.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DirectivesComponent
+    DirectivesComponent,
+    NewCompComponent
   ],
   imports: [
     BrowserModule,
---
+++ angularTraining/first-app/src/app/new-comp/new-comp.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-comp',
+  templateUrl: './new-comp.component.html',
+  styleUrls: ['./new-comp.component.css']
+})
+export class NewCompComponent implements OnInit {
+
+  num = 10
+  sum = 20
+  placeholder = "Enter Your Father's Name"
+  disabled:boolean = true;
+
+  strClass = "active class1"
+  objClass = {
+    active: true,
+    class2: false
+  }
+  arrClass = ['class3']
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  myfunc() {
+    return "Aakash Pawan"
+  }
+
+  clicked(){
+    alert("Button is Clicked")
+  }
+}
