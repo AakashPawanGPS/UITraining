4e1e49967da306f263ca52927268764bac9f65a6
AakashPawanGPS <aakashpawangps@gmail.com>
2022-09-05 10:46:39 +0530

    ng Directives

---
+++ angularTraining/first-app/src/app/directives/directives.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-directives',
+  template: './directives.component.html',
+  styleUrls: ['./directives.component.css']
+})
+export class DirectivesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
